f6f1ec077b852dd740c7976bedbf49c96affb413
MkSchmidt <misha.schmidtz@gmail.com>
2025-05-01 13:22:38 +0200

    data test

---
+++ datatest.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+import re
+
+project_root = os.path.dirname(__file__)
+
+def load_netfile(name):
+    netfile = os.path.join(project_root, "TransportationNetworks", name, f"{name}_net.tntp")
+    net = pd.read_csv(netfile, skiprows=8, sep='\t')
+
+    trimmed= [s.strip().lower() for s in net.columns]
+    net.columns = trimmed
+
+    # And drop the silly first andlast columns
+    net.drop(['~', ';'], axis=1, inplace=True)
+    
+    # Metadata
+    with open(netfile, "r") as f:
+        attrs = re.findall("(?m)^\\s*\\<(.*)\\>\\s*(.*\\S)\\s*$", f.read())
+
+    net.attrs = dict(attrs)
+    return net
+
+anaheim = load_netfile("Anaheim")
+
+
