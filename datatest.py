a614466848b8767240732b29397830a9827d2776
LennardHol <lennard.hol@gmail.com>
2025-05-01 16:39:50 +0200

    Update datatest.py

--- datatest.py
+++ datatest.py
@@ -4,15 +4,32 @@ import re
 
 project_root = os.path.dirname(__file__)
 
+def load_nodefile(name):
+    nodefile = os.path.join(project_root, "TransportationNetworks", name, f"{name}_node.tntp")
+    node = pd.read_csv(nodefile, sep='\t')
+
+    trimmed= [s.strip().lower() for s in node.columns]
+    node.columns = trimmed
+
+    # And drop the silly first andlast columns
+    node.drop([';'], axis=1, inplace=True)
+    
+    # Metadata
+    with open(nodefile, "r") as f:
+        attrs = re.findall("(?m)^\\s*\\<(.*)\\>\\s*(.*\\S)\\s*$", f.read())
+
+    net.attrs = dict(attrs)
+    return node
+
 def load_netfile(name):
-    netfile = os.path.join(project_root, "TransportationNetworks", name, f"{name}_node.tntp")
-    net = pd.read_csv(netfile, sep='\t')
+    netfile = os.path.join(project_root, "TransportationNetworks", name, f"{name}_net.tntp")
+    net = pd.read_csv(netfile, skiprows=8 , sep='\t')
 
     trimmed= [s.strip().lower() for s in net.columns]
     net.columns = trimmed
 
     # And drop the silly first andlast columns
-    net.drop([';'], axis=1, inplace=True)
+    net.drop(['~',';'], axis=1, inplace=True)
     
     # Metadata
     with open(netfile, "r") as f:
@@ -21,5 +38,7 @@ def load_netfile(name):
     net.attrs = dict(attrs)
     return net
 
-SiouxFalls = load_netfile("SiouxFalls")
-print(SiouxFalls)
+net = load_netfile("SiouxFalls")
+node = load_nodefile("SiouxFalls")
+print(net,node)
+
