9a6f4a5ae41a83151260082e94a27d9f731c8d3a
MkSchmidt <misha.schmidtz@gmail.com>
2025-05-02 11:46:10 +0200

    added folder reading

--- read_files.py
+++ read_files.py
@@ -1,12 +1,12 @@
 import os
 import pandas as pd
 import re
+import json
 
 project_root = os.path.dirname(__file__)
 
-def load_nodefile(name):
-    nodefile = os.path.join(project_root, "TransportationNetworks", name, "friedrichshain-center_node.tntp")
-    node_df = pd.read_csv(nodefile, sep=r"\s+", index_col="Node")
+def load_nodefile(path):
+    node_df = pd.read_csv(path, sep=r"\s+", index_col="Node")
 
     trimmed = [s.strip().lower() for s in node_df.columns]
     node_df.columns = trimmed
@@ -16,23 +16,51 @@ def load_nodefile(name):
 
     return node_df
 
-def load_edgefile(name):
-    edgefile = os.path.join(project_root, "TransportationNetworks", name, "friedrichshain-center_net.tntp")
-    edge_df = pd.read_csv(edgefile, skiprows=8, sep='\t')
+def load_edgefile(path):
+    edge_df = pd.read_csv(path, skiprows=8, sep='\t')
 
     trimmed = [s.strip().lower() for s in edge_df.columns]
     edge_df.columns = trimmed
 
     edge_df.drop(['~', ';'], axis=1, inplace=True)
 
-    with open(edgefile, "r") as f:
+    with open(path, "r") as f:
         attrs = re.findall(r"(?m)^\s*<(.*)>\s*(.*\S)\s*$", f.read())
     edge_df.attrs = dict(attrs)
 
-    print("✅ Edges:")
-    print(edge_df)
     return edge_df
 
-# Load both files
-nodes = load_nodefile("Berlin-Friedrichshain")
-edges = load_edgefile("Berlin-Friedrichshain")
+def load_nodefile_geojson(path):
+    with open(path, "r") as f:
+        node_dict = json.load(f)
+
+    nodes = [
+                {
+                    "node": point["properties"]["id"],
+                    "x": point["geometry"]["coordinates"][0],
+                    "y": point["geometry"]["coordinates"][1]
+                } for point in node_dict["features"]
+            ]
+    return pd.DataFrame.from_records(nodes)
+
+def read_folder(folder_path):
+    files = os.listdir(folder_path)
+    edgefiles = list(filter(lambda name: re.match(r".*_net\.tntp$", name), files))
+    assert len(edgefiles) > 0, "TNTP voor het net moet bestaan"
+    edge_path = os.path.join(folder_path, edgefiles[0])
+    edges = load_edgefile(edge_path)
+
+    nodefiles_tntp = list(filter(lambda name: re.match(r".*_node\.tntp$", name), files))
+    if len(nodefiles_tntp) > 0:
+        node_path = os.path.join(folder_path, nodefiles_tntp[0])
+        nodes = load_nodefile(nodefiles_tntp[0])
+    else:
+        nodefiles_geojson = list(filter(lambda name: re.match(r".*_nodes\.geojson$", name), files))
+        assert len(nodefiles_geojson) > 0, "node file moet bestaan"
+        node_path = os.path.join(folder_path, nodefiles_geojson[0])
+        nodes = load_nodefile_geojson(node_path)
+
+    return edges, nodes
+
+
+
