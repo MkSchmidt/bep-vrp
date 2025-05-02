366daf2d2bbcb4e85114cf45a9a19b4cd8d9febe
MkSchmidt <misha.schmidtz@gmail.com>
2025-05-02 15:08:50 +0200

    Alle bestanden lezen

--- read_files.py
+++ read_files.py
@@ -45,22 +45,34 @@ def load_nodefile_geojson(path):
 
 def read_folder(folder_path):
     files = os.listdir(folder_path)
-    edgefiles = list(filter(lambda name: re.match(r".*_net\.tntp$", name), files))
+    edgefiles = list(filter(lambda name: re.match(r"(?i).*_net\.tntp$", name), files))
     assert len(edgefiles) > 0, "TNTP voor het net moet bestaan"
     edge_path = os.path.join(folder_path, edgefiles[0])
     edges = load_edgefile(edge_path)
 
-    nodefiles_tntp = list(filter(lambda name: re.match(r".*_node\.tntp$", name), files))
+    nodefiles_tntp = list(filter(lambda name: re.match(r"(?i).*_node\.tntp$", name), files))
     if len(nodefiles_tntp) > 0:
         node_path = os.path.join(folder_path, nodefiles_tntp[0])
         nodes = load_nodefile(node_path)
     else:
-        nodefiles_geojson = list(filter(lambda name: re.match(r".*_nodes\.geojson$", name), files))
+        nodefiles_geojson = list(filter(lambda name: re.match(r"(?i).*_nodes\.geojson$", name), files))
         assert len(nodefiles_geojson) > 0, "node file moet bestaan"
         node_path = os.path.join(folder_path, nodefiles_geojson[0])
         nodes = load_nodefile_geojson(node_path)
 
     return edges, nodes
 
-
+def test_folder_reading():
+    network_dir = os.path.join(project_root, "TransportationNetworks")
+    
+    for item in os.listdir(network_dir):
+        item_path = os.path.join(network_dir, item)
+        if os.path.isfile(item_path): continue
+        
+        try:
+            read_folder(item_path)
+        except AssertionError as e:
+            print(f"Shit foutgegaan in mapje {item}: {str(e)}")
+        except Exception as e:
+            print(f"ANDERE shit foutgegeaan in mapje {item}: {str(e)}")
 
