cecbe50416e6080017acc8a7493f57ea2e01a6c9
lrgooijer <luuksbrievenbus@gmail.com>
2025-05-21 12:27:01 +0200

    demand functie , skelet flow en density

--- sim_vrp_no_paths.py
+++ sim_vrp_no_paths.py
@@ -1,10 +1,14 @@
 import networkx as nx
 import pandas as pd
 from typing import Optional
+import os
+from read_files import load_edgefile, load_flowfile, load_nodefile, project_root
+import matplotlib.pyplot as plt
+from matplotlib import animation
 
 customers = [918, 911, 500, 400 , 300, 600] #Assign Customer location
 depot = [10] #Assign Depot Location
-demand_value = [5]
+demand_value = [5] # Assign demand
 
 # Read Files
 def graph_from_data(edges: pd.DataFrame, nodes: Optional[pd.DataFrame] = None) -> nx.DiGraph:
@@ -22,6 +26,13 @@ def graph_from_data(edges: pd.DataFrame, nodes: Optional[pd.DataFrame] = None) -
     ]
     graph.add_edges_from(edge_list)
     return graph
+
+# Load Data
+
+edges_df: pd.DataFrame =  load_edgefile(os.path.join(project_root, "TransportationNetworks", "Chicago-Sketch", "ChicagoSketch_net.tntp"))
+nodes_df = load_nodefile(os.path.join(project_root, "TransportationNetworks", "Chicago-Sketch", "ChicagoSketch_node.tntp"))
+flow_df = load_flowfile(os.path.join(project_root, "TransportationNetworks", "Chicago-Sketch", "ChicagoSketch_flow.tntp"))
+
 
 # Give current time as minutes since midnight
 def get_added_travel_time(edge_attributes, t):
@@ -44,31 +55,96 @@ def get_added_travel_time(edge_attributes, t):
 
     return max(0, hump)
 
+
 def get_travel_time(edge_attributes, t):
     added_time = get_added_travel_time(edge_attributes, t)
     return edge_attributes.get("free_flow_time", 0) + added_time
 
-# Assign a demand to Customers
+# Assign a Demand to Customers
 
 def assign_customer_demands(customers,demand_value):
     demands = {}
     for customer in customers:
-        demands[customer] = demand_value
-    
+        demands[customer] = demand_value 
     return demands
+
+# Parameters
+ # w = congestion_speed
+ # qc = max_flow = capacity
+ # pc = crit_density
+ # uf = freeflowtime
+ # tt = traveltime
+ # q = flow
+
+# Demand functie (___/----\____ vormig)
+t1 = 6.5 * 60   # 7:00
+t2 = 8.5 * 60   # 8:30
+t3 = 10 * 60    # 10:00
+t4 = 12 * 60
+t5 = 16.5 * 60  # 16:30
+t6 = 18 * 60  # 17:30
+t7 = 20 * 60    # 20:00
+t8 = 22 * 60    # 21:00
+
+def demand(t, t1, t2, t3, t4):
+    low,medium, high = 0.1, 0.5, 1.1
+
+    if t <= t1:
+         return low
+    elif t < t2:
+        return low + (high - low)*(t - t1)/(t2 -t1)
+    elif t < t3:
+        return high
+    elif t <= t4:
+        return high - (high - medium)*(t - t3)/(t4 - t3)
+    elif t <= t5:
+        return medium
+    elif t < t6:
+        return medium + (high - medium)*(t - t5)/(t6 - t5)
+    elif t <= t7:
+        return high
+    elif t < t8:
+        return low + (high -low)*(1- (t -t7)/(t8 -t7))
+    else:
+        return low
+
+t_values = range(24*60)                   # 0…1439 minutes
+demand_profile = [demand(t, t1,t2,t3,t4) for t in t_values]
+
+'''
+# Calculate the Flow of each edge 
+def flow(edges_df, demand_profile, t_current):
+    for edge in edges_df:
+        volume = edge_df.get("volume", 0.0)
+        flow_series = [volume * d for d in demand_profile]
+        edge["flow_profile"] = flow_series
+    return edges_df
+
+
+t_current =  13*60 
+edges_df = flow(edges_df, demand_profile, t_current)
+third_edge = edges_df[2]
+print(third_edge["flow_current"])
+
+
+
+# Density bepalen
+def density():
+    pc = qc / uf # crit_density = max_flow / freeflowtime
+    pj = # ???? hoe bereken/bepaal je dit ???
+    w = qc / (pj - pc) # Congestion speed  
+ 
+    if density =< crit_density:
+        density = flow / freeflowtime
+    else density > crit_density:
+            density = flow / congestion speed
+    return 
+'''
+
 
 
 if __name__ == "__main__":
-    import os
-    from read_files import load_edgefile, load_flowfile, load_nodefile, project_root
-    import matplotlib.pyplot as plt
-    from matplotlib import animation
 
-    # Load data
-    edges_df: pd.DataFrame =  load_edgefile(os.path.join(project_root, "TransportationNetworks", "Chicago-Sketch", "ChicagoSketch_net.tntp"))
-    nodes_df = load_nodefile(os.path.join(project_root, "TransportationNetworks", "Chicago-Sketch", "ChicagoSketch_node.tntp"))
-    flow_df = load_flowfile(os.path.join(project_root, "TransportationNetworks", "Chicago-Sketch", "ChicagoSketch_flow.tntp"))
-    
     # Create directed graph from data
     G_directed = graph_from_data(edges_df, nodes=nodes_df)
     
@@ -135,3 +211,11 @@ if __name__ == "__main__":
     plt.ylabel("Y coordinate")
     fig.tight_layout()
     plt.show()
+
+fig, ax = plt.subplots()
+ax.plot(t_values, demand_profile)
+ax.set_xlabel('Time (minutes since midnight)')
+ax.set_ylabel('Demand')
+ax.set_title('Daily Demand Profile')
+plt.tight_layout()
+plt.show()
