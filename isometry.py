8231dfbf1e2ecc7f886c36e9a73d33d34cad53c5
MkSchmidt <misha.schmidtz@gmail.com>
2025-05-22 11:23:08 +0200

    added shit

--- isometry.py
+++ isometry.py
@@ -27,10 +27,29 @@ def place_points(squared_distances):
 
     return coordinates
 
-def distances_from_points(points):
+def squared_distances_from_points(points):
     n = points.shape[0]
     differences = points.reshape(1, n, -1) - points.reshape(n, 1, -1)
     return torch.sum(differences**2, dim=2)
+
+def mse(points, squared_distances):
+    point_distances = torch.sqrt(squared_distances_from_points(points))
+    d = point_distances - torch.sqrt(squared_distances)
+    squared_error = torch.abs(d)**2
+    return torch.mean(torch.triu(squared_error, diagonal=1))
+
+def approximate_points(squared_distances, iterations=100, lr=1e-3):
+    points = torch.rand((NUM_VERTICES, NUM_VERTICES - 1), dtype=torch.complex64, requires_grad = True)
+
+    adam = torch.optim.Adam([points], lr=lr)
+    mse_history = []
+    for i in range(iterations):
+        loss = mse(points, squared_distances)
+        loss.backward()
+        adam.step()
+        mse_history.append(loss.detach().item())
+
+    return points, mse_history
 
 if __name__ == "__main__":
     NUM_VERTICES = 10
@@ -39,7 +58,7 @@ if __name__ == "__main__":
     squared_distances = torch.abs(squared_distances - squared_distances.T)
     
     analytical_points = place_points(squared_distances)
-    
-    random_points = torch.rand((NUM_VERTICES, NUM_VERTICES), dtype=torch.complex64)
 
-    pairwise_d = lambda i, j: torch.sum((analytical_points[i] - analytical_points[j])**2)
+    random_points = torch.rand((NUM_VERTICES, NUM_VERTICES), dtype=torch.complex64)
+    
+    approx_points, mse_history = approximate_points(squared_distances)
