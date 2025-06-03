2b61a323508b1e00a5edbfaab59912203ceeb036
lrgooijer <luuksbrievenbus@gmail.com>
2025-06-03 10:38:42 +0200

    hgjg

--- sioux_falls_test.py
+++ sioux_falls_test.py
@@ -1,8 +1,13 @@
 from write_sumo_net import convert_folder
 import os
 
+# Maps to choose:
+# Simple :          SiouxFalls
+# Intermediate :    Anaheim
+# Complex :         Chicago-Sketch
+
 if __name__ == "__main__":
     project_root = os.path.dirname(__file__)
-    input_folder = os.path.join(project_root, "TransportationNetworks", "SiouxFalls")
+    input_folder = os.path.join(project_root, "TransportationNetworks", "Anaheim")
     output_folder= os.path.join(project_root, "output")
     convert_folder(input_folder, output_folder)
