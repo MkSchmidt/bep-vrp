97fb0c12e6dc619b9190e71367f136e0d18e7f21
MkSchmidt <misha.schmidtz@gmail.com>
2025-06-04 14:37:26 +0200

    cleanup

--- sioux_falls_test.py
+++
@@ -1,8 +0,0 @@
-from write_sumo_net import convert_folder
-import os
-
-if __name__ == "__main__":
-    project_root = os.path.dirname(__file__)
-    input_folder = os.path.join(project_root, "TransportationNetworks", "Anaheim")
-    output_folder= os.path.join(project_root, "output")
-    convert_folder(input_folder, output_folder)
---
+++ vrp_sim.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+from typing import Optional
+import networkx as nx
+from itertools import pairwise
+
+class TrafficSim:
+    def __init__(self, edges: pd.DataFrame, nodes: Optional[pd.DataFrame] = None):
+        self.G = nx.DiGraph()
+        if nodes is not None:
+            coords = {row["node"]: (row["x"], row["y"]) for row in nodes.to_dict('records')}
+            for node, xy in coords.items():
+                self.G.add_node(node, coordinates=xy)
+        for row in edges.to_dict('records'):
+            self.G.add_edge(row["init_node"], row["term_node"], **row)
+    
+    def get_route_travel_time(self, route: list[int], t_start: float = 3600*8) -> float:
+        head = self._get_edge_travel_time(route[0], route[1], t_start)
+        if len(route) > 2:
+            tail = self.get_route_travel_time(route[1:], t_start + head)
+        else:
+            tail = 0
+        return head + tail
+
+    def get_solution_travel_time(self, solution: list[list[int]], t_start: int = 3600*8) -> tuple[float, float]:
+        travel_times = [ self.get_route_travel_time(route, t_start) for route in solution ]
+        total = sum(travel_times)
+        maximum = max(travel_times)
+        return total, maximum
+
+    def _get_edge_travel_time(self, source: int, dest: int, t: float) -> float:
+        attrs = self.G.edges[source, dest]
+        free_time = attrs.get("free_flow_time") * 60
+        critical_density = self._get_critical_density(source, dest)
+        density = self._get_density(attrs, t_min)
+        flow = self._get_flow(attrs, t_min)
+        if density <= critical_density:
+            travel_time = free_time
+        else:
+            travel_time = free_time + (density - critical_density) * B
+        return travel_time
+    
+    def _get_density(self, source: int, dest: int, t: float) -> float:
+        attrs = self.G.edges[source, dest]
+        length = attrs.get("length") * 0.3048
+        free_time = attrs.get("free_flow_time") * 60 or length / (50*3.6)
+        flow = _get_flow(attrs, t_min)
+        free_speed = (length / free_time)
+        return flow / free_speed
+
+    def _get_flow(self, source: int, dest: int, t: float) -> float:
+        volume = self.G.edges[source, dest].get("volume") or 0
+        return volume * self._demand(t)
+    
+    def _get_critical_density(self, source: int, dest: int) -> float:
+        attrs = self.G.edges[source, dest]
+        capacity = attrs.get("capacity") * 3600
+        free_time = attrs.get("free_flow_time") * 60
+        length = attrs.get("length") * 0.3048
+        ff_speed = length / free_time or 50 * 3.6
+        return capacity / ff_speed
+
+    # Demand function
+    def _demand(self, t: float) -> float:
+        low, medium, high = 0.1, 0.5, 1.1
+        demands = np.array([ low, low, high, high, medium, medium, high, high, low, low ])
+        times =   np.array([ 0,   4,   8.5,  10,   12,     16.5,   18,   20,   22,  24  ]) * 3600    
+        
+        return np.interp(t, times, demands)
+
+class VRP:
+    def __init__(self, depot: int, customers: list[int]):
+        self.customers = set(customers)
+        self.depot = depot
+
+    def check_solution_valid(self, solution: list[list[int]]) -> bool:
+        customers_visited = set()
+
+        for route in solution:
+            if route[0] != self.depot or route[-1] != self.depot:
+                return False
+            route_set = set(route)
+            if len(customers_visited & route) > 0:
+                return False
+            customers_visited = customers_visited & route - {depot,}
+
+        return len(self.customers ^ customers_visited) == 0
